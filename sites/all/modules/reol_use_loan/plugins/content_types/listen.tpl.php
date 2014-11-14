c8aa3aa8db4b5b57a4d50907523d30cf35ec2e17
Michael Søby Andersen <michael@megha.it>
2014-11-14 13:23:22 +0100

    Changed listener to use distributed one instead. REOL-11

--- sites/all/modules/reol_use_loan/plugins/content_types/listen.tpl.php
+++ sites/all/modules/reol_use_loan/plugins/content_types/listen.tpl.php
@@ -6,23 +6,7 @@
  * to an audio book.
  */
 
-$path = drupal_get_path('module', 'reol_use_loan');
-$player_path = $path . '/player';
-
-drupal_add_css($player_path . '/lib/bootstrap-3.2.0-dist/css/bootstrap.min.css');
-drupal_add_css($player_path . '/css/styles.css');
-
-drupal_add_js($path . '/js/jquery-define.js');
-drupal_add_js($player_path . '/scripts/bootstrap-slider.js');
-drupal_add_js($player_path . '/scripts/bowser.min.js');
-drupal_add_js($player_path . '/scripts/tooltip.js');
-drupal_add_js($player_path . '/scripts/modernizr2.7.1.min.js');
-drupal_add_js($player_path . '/scripts/dragdealer.js');
-drupal_add_js($player_path . '/scripts/player-ui-default-1.0.2.js');
-drupal_add_js($player_path . '/scripts/player-kernel-1.0.3.min.js');
-drupal_add_js($path . '/js/player.js');
+drupal_add_css(drupal_get_path('module', 'reol_use_loan') . '/css/player.css');
 ?>
 
-<div data-role="audiobook-player" data-id="<?php echo $internal_order_number; ?>" style="margin-top: 0 !important">
-
-</div>
+<iframe src="<?php echo variable_get('publizon_player_url', ''); ?>/?o=<?php echo $internal_order_number; ?>&k=<?php echo $player_client_id; ?>" class="player"></iframe>
