b01f8128c6e6d2e08ca75d8dd8906ab52c420b36
Michael Søby Andersen <michael@megha.it>
2014-11-27 13:12:09 +0100

    Changed some styling on article teaser. REOL-68

--- sites/all/themes/brin/templates/node--article--view-mode--teaser.tpl.php
+++ sites/all/themes/brin/templates/node--article--view-mode--teaser.tpl.php
@@ -7,15 +7,17 @@
 ?>
 
 <div class="article-teaser">
-  <div class="article-left">
-    <?php echo render($content['field_ding_news_list_image']); ?>
-  </div>
-  <div class="article-right">
-    <div class="article-background">
-      <?php echo render($image_background); ?>
+  <a href="<?php echo $readmore; ?>">
+    <div class="article-left">
+      <?php echo render($content['field_ding_news_list_image']); ?>
     </div>
-    <h2><?php echo $title; ?></h2>
-    <div class="article-body"><?php echo render($content['body']); ?></div>
-    <div class="read-more"><?php echo render($content['links']); ?></div>
-  </div>
+    <div class="article-right">
+      <div class="article-background">
+        <?php echo render($image_background); ?>
+      </div>
+      <h2><?php echo $title; ?></h2>
+      <div class="article-body"><?php echo render($content['body']); ?></div>
+      <div class="read-more"><?php echo $readmore_text; ?></div>
+    </div>
+  </a>
 </div>
