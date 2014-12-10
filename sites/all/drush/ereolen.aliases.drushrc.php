d844a5a247767cf878cc633b6ef5eb03a08911f9
Thomas Fini Hansen <fini@reload.dk>
2014-12-10 14:15:10 +0100

    Need ForwardAgent too.

--- sites/all/drush/ereolen.aliases.drushrc.php
+++ sites/all/drush/ereolen.aliases.drushrc.php
@@ -91,7 +91,7 @@ $aliases['test-test'] = array(
 // The server only allows certain IPs to log in, so we hack up an
 // alias for the jump server here.
 $aliases['circle'] = $aliases['p01'];
-$aliases['circle']['ssh-options'] .= " -o 'ProxyCommand  ssh circleci@ding.reload.dk nc %h %p'";
+$aliases['circle']['ssh-options'] .= " -o ForwardAgent=yes -o 'ProxyCommand  ssh circleci@ding.reload.dk nc %h %p'";
 
 $aliases['circle-dev'] = $aliases['dev'];
 $aliases['circle-dev']['parent'] = '@circle';
