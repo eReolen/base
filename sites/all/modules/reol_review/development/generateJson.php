6796c5be2ec8a0611fcb2cb996891308d0179868
Michael Søby Andersen <michael@megha.it>
2014-12-16 11:23:00 +0100

    Changed data in overridden feed to actually be relevant. REOL-118

--- sites/all/modules/reol_review/development/generateJson.php
+++ sites/all/modules/reol_review/development/generateJson.php
@@ -3,8 +3,18 @@
 $items = array(
   array(
     'nid' => '100',
-    'title' => 'Anmelder Det Første Svar',
-    'description' => 'Vi har anmeldt denne fine bog',
+    'title' => 'Stalker af Lars Kepler',
+    'description' => '<p><strong>Succes-ægteparret leverer igen et mesterligt plot med forrygende og isnende uhygge samt et forfriskende gensyn med psykologen Erik Maria Bark.</strong></p>
+
+<p>Et link til et klip på Youtube, som viser en kvinde i færd med at tage strømpebukser på, sendes til Rikskrim, men ingen ved politiet tager mailen alvorligt. Ikke før kvinden findes myrdet på groveste vis af knivstik. Endnu en filmsekvens bliver sendt, og den nye kommissær Margot Silverman, der nu sidder i Joona Linnas stol, har kun kort tid til at få identificeret kvinden og få hende advaret - forgæves skal det vise sig. Den myrdede kvindes mand, som har fundet liget, blev så chokeret over at finde sin kone på den måde, at han flyttede liget og forsøgte at gøre rent.</p>
+
+<p>For at finde ud af, hvad han har set, kontakter Margot psykologen Erik Maria Bark. Erik forsøger sig med hypnose, men det specielle forhør bringer ubehagelige minder frem hos ham. Et lignende mord blev begået for ni år siden, og alle spor pegede mod en tidligere præst, som blev dømt. Erik var ligeså overbevist om manden skyld som alle andre, så derfor tav han om et sandsynligt alibi. Men de nye mord sammenholdt med den gamle sag får ham nu til at begynde sin egen efterforskning.</p>
+
+<p>Han får uventet hjælp af Joona Linna, som alle ellers troede var død. Joona var gået i skjul sammen med sin familie, fordi han stadig følte sig truet af Jurek Walter (’Sandmanden’), men da Joonas tidligere kollega Saga Bauer omsider finder det endegyldige bevis på Walters død, kommer Joona frem fra sit skjul igen. Flere bestialske mord følger, og hvad værre er: Alle spor synes at pege i retning af Bark</p>
+
+<p>Lars Kepler er pseudonym for ægteparret Ahndoril, og deres nu fem romaner om Joona Linna har i den grad placeret dem helt øverst på skalaen for medrivende og intelligente spændingsbøger. Med en ’Kepler’ kan man altid være sikker på at få en tidsrøvende krimi, der er næsten umulig at lægge fra sig. Forfatterparret kan da også deres kram til fingerspidserne, og som læser tages man den ene gang efter den anden ved næsen og føres i nye overraskende retninger. Den mundrette tekst, de korte kapitler med skiftende fokus på de forskellige personer, det indviklede og medrivende plot og de engagerede personskildringer går alle op i en højere enhed.</p>
+
+<p>Selvom man sagtens kan læse den nye historie uafhængigt af de forrige bind, så er der dog efterhånden så mange tilbageblik på tidligere begivenheder og personer, at man kun med et godt kendskab til hele serien får den ultimative oplevelse. Og så skal det også lige for en god ordens skyld nævnes, at dette femte bind efter en forrygende indledning går lidt i stå midt i bogen, men så sandelig kommer tilbage på spændingssporet i den sidste tredjedel. Det korte af det lange er, at det alligevel ender med at være fandens god underholdning.</p>',
     'isbn' => '9788792539731',
     'credate' => '1416577066',
     'author' => 'Michael Søby Andersen',
@@ -13,8 +23,26 @@ $items = array(
   ),
   array(
     'nid' => '101',
-    'title' => 'Nu er det Bortført',
-    'description' => 'Vi har anmeldt endnu en fin bog',
+    'title' => 'At elske eller ikke at elske - Nye og gamle kærlighedsdigte af Benny Andersen',
+    'description' => '<p><strong>En perle af en digtsamling, der taler til både hjerte og hjerne.</strong></p>
+
+<p>I anledning af sin 85-års fødselsdag udgiver Benny Andersen et udvalg af sine bedste kærlighedsdigte fra tidligere samlinger sammen med en række nyskrevne digte. Samlingen har fået den shakespeareske titel <em>At elske eller ikke at elske</em>, og når man læser digtene, er man – hvis man ikke var det i forvejen – overbevist om, at det at elske for Benny Andersen er det sammen som at være.</p>
+
+<p>De ældste af digtene fra 1960’erne og 1970’erne bærer præg af at være skrevet i en modernistisk tradition, mens de nyere digte er mere lyriske, men den sproglige finurlighed er et kendetegn hele vejen igennem. Benny Andersen kan noget med sproget. Hvem andre ville finde på at lave bøjningsformen: Hjem, hjemme, hjemmere, hjemmest? Eller verslinjen: Lettede lettede vi sammen?</p>
+
+<p>Hos Benny Andersen rimer hjerte på smerte, men naturligvis leger han med begreberne som i digtet ’Til min hjernes udkårne’, hvor han skriver hjerne der, hvor der burde have stået hjerte. Første gang jeg læste digtet, opdagede jeg det overhovedet ikke - for mit indre blik stod der hjerte. Digtet er i øvrigt skrevet til hjerneåret 1997.</p>
+
+<p>Benny Andersen lægger ikke skjul på, at han elsker kvinden – både kvinden som begreb og den kvinde, som han til enhver tid lever sammen med. Mange af digtene er skrevet til hans Cynthia Rosalina fra Barbados, ’Mit hjertes indvandrer’, som han kalder hende. Et af de nye digte er henvendt til hustruen Elisabeth og hedder ’Kærlig bønskift til E.’ Et bittersødt digt, hvor han siger: ”Jeg beder dig så inderligt: dø ikke fra mig!”</p>
+
+<p>Digtene kan virke, som var de uden kant, men under overfladen lurer den til tider skarpe kritik af småborgerlighed og intolerance. Man ser det tydeligere i andre af Benny Andersens digte end i kærlighedsdigtene. Men kritikken findes – fint og nænsomt pakket ind i det harmløse. Mange af digtene er ret sanselige, og selv om man næppe vil beskylde den pæne gamle mand for at skrive erotisk lyrik, tenderer nogle af de nyere digte. I det sidste digt, der hedder ’Erotik-taktik’, resulterer lagengymnastikken således i mere sæd end vane!</p>
+
+<p>Benny Andersen vender flere gange tilbage til de samme billeder, således blev hans hånd stamgæst hos den elskede i ’Stjerneklart’ fra 1962, og de elskende bliver stamgæster hos hinanden i ’Livslinjer’ fra 2005. Når jeg læser om stamgæsten, kommer jeg uvilkårligt til at tænke på filmen <em>Man sku’ være noget ved musikken</em>, som Benny Andersen har været manuskriptforfatter til. Er det mon et tilfælde?</p>
+
+<p>Jeg stiftede bekendtskab med Benny Andersen, da en progressiv dansklærer præsenterede mig for samlingen <em>Her i reservatet</em> i 1971, og jeg var - som så mange andre - solgt! Benny Andersen har formået det, som de fleste lyrikere – bort set fra Yahya Hassan - må misunde ham: Nemlig at komme på bestsellerlisterne. Det er velfortjent, for Benny Andersen har vist, at lyrik ikke er en elitær kunstart.</p>
+
+<p>Jeg vil elske at vende tilbage til denne digtsamling igen og igen, om ikke andet så for at se på det smukke omslag: Et knaldrødt hjerte på skrigende pink baggrund.&nbsp;</p>
+
+<p>Originally published by Anne Klara Bæhr, Litteratursiden.</p>',
     'isbn' => '9788792639318',
     'credate' => '1416577066',
     'author' => 'Thomas Fini Hansen',
@@ -23,8 +51,24 @@ $items = array(
   ),
   array(
     'nid' => '102',
-    'title' => 'Så har vi Det Gode Håb',
-    'description' => 'Vi har anmeldt en lydbog!',
+    'title' => 'Britt-Marie var her af Fredrik Backman',
+    'description' => '<p><strong>Skildringen af Britt-Marie og den lille flække Borg er fuldt på højde med forgængeren <em>En mand der hedder Ove</em> - underholdende, humoristisk, og rørende.</strong></p>
+
+<p>63-årige Britt-Marie er fra storbyen. Hun er gift med Kent og har i snart fyrre år gået hjemme og passet ham og rengøringen. Kent er en succesrig entreprenør med gode forretninger især i Tyskland, og han har længe haft noget kørende med en yngre model. Da Kent får et hjerteanfald, er det faktisk elskerinden, der underretter Britt-Marie, og det er lige denne detalje, der får Britt-Marie til at sige stop.</p>
+
+<p>Gennem arbejdsformidlingen får hun et job i et fritidshjem i den lille by Borg, som virkelig er prototypen på ’Udkantssverige’. Alle forretninger og virksomheder er stort set lukket, ligesom fritidshjemmet i øvrigt også skulle have været det, og tilbage er kun en masse til salg-skilte og et pizzeria, der samtidig fungerer som både købmand, posthus og mekanikerværksted.</p>
+
+<p>Det eneste vigtige, som både børn og voksne i byen brænder for, er fodbold, og Britt-Marie kan overhovedet ikke fordrage fodbold.</p>
+
+<p>Men Britt-Marie er ikke typen, der sådan giver op, og selvom hun slet ikke har været ude i samfundet i så mange år, beslutter hun at give byen en chance. Mødet med Borg-boerne, fodbolden og børnene kommer til at vende op og ned på alt, hvad der er Britt-Marie, og hverken hun eller byen bliver nogensinde den samme igen. Fredrik Backman bragede igennem sidste år med den fantastiske <em>En mand der hedder Ove</em>, og sandelig om han ikke&nbsp;igen har skrevet en helt vidunderlig og bemærkelsesværdig roman. <em style="font-size: 16px;">Britt-Marie var her</em> rangerer igen meget, meget højt på listen over årets bedste bøger.</p>
+
+<p>Historien er fabelagtig, også selvom man overhovedet ikke skulle have noget til overs for fodbold, og man sidder skiftevis og klukler og morer sig over de komiske situationer og beskrivelser, og skiftevis er man lige nødt til at knibe en lille tåre. Bogen spiller på hele følelsesregisteret, og udover at man bliver særdeles godt underholdt, så giver indholdet også rig mulighed for eftertænksomhed og selvransagelse.</p>
+
+<p>Sprogligt er bogen meget direkte og let at gå til, men alligevel formår forfatteren at tegne et knivskarpt billede af sine personer – især af Britt-Marie naturligvis, som i den grad fremstår så irriterende i starten, at det næsten er for meget, og som alligevel ender med at blive en person, man kommer til at holde af. På trods af bogens lettilgængelighed og humorens form formår forfatteren oven i købet at få leveret en indfølende og magisk beskrivelse af et lille samfund midt i finanskrisen, og på det psykologiske plan bliver der oven i købet plads til et par eftertænksomme indspark til, at man skal leve livet, mens man har det.</p>
+
+<p>Snyd ikke dig selv for denne bog.</p>
+
+<p>Originally published by Arne Larsen, Litteratursiden.</p>',
     'isbn' => '9788792165473',
     'credate' => '1416577066',
     'author' => 'Rasmus Lückow Nielsen',
@@ -33,8 +77,28 @@ $items = array(
   ),
   array(
     'nid' => '103',
-    'title' => 'Endnu en lydbog: Det Første Jeg Tænker På',
-    'description' => 'En lydbog igen',
+    'title' => 'De elskende – et sammenstød af Christel Wiinblad',
+    'description' => '<p><strong>Stærk og foruroligende. Christel Wiinblad skriver sig helt ind i sjælens mørkeste kringelkroge og drift imod undergang og udslettelse.</strong></p>
+
+<p>Kærlighed kan være svær og opslidende. Nogle gange kan den ligefrem være ødelæggende. I Christel Wiinblads <em>De elskende</em> er kærligheden meget mere end det.</p>
+
+<p>”Jeg tror nærmest, jeg virkelig er ved at dø af det”, udtaler den kvindelige hovedperson igen og igen igennem Christel Wiinblads nyeste skildring af den sårbare menneskelige psyke, hvor trangen til selvudslettelse og den naturlige fysiske overlevelsesmekanisme er to uforenelige drifter, der kæmper om at få overtaget og hele tiden støder sammen inden i hovedpersonen.</p>
+
+<p>Handlingen (som strengt taget mere er en ikke-handling) udspiller sig hen over et år i en hastigt nedadgående spiral henimod den fuldstændige underkastelse, og - som en konsekvens heraf – den totale udslettelse af den kvindelige hovedperson. Og det i en grad så romanens sprog direkte påvirkes af det. Imens hovedpersonen gradvist lader sin krop udslette, udslettes hendes forståelse af verdens materialitet også.</p>
+
+<p>Det er stærk kost. Og utrolig svært - som læser - at være vidne til. Manden, som hun underkaster sig, er dæmonisk. Ren og smuk og kold. Og konsekvent i sit udtalte ønske om at overskride og nedbryde hendes fysiske materielle verden for at nå noget af betydning: ”Det er smukkere end alt”, sagde han. Alt skal gå under i et mægtigt lys. Intet andet betyder noget. Kærligheden overvinder alt. Jeg vil vise dig det. Jeg vil tage dig hele vejen igennem det.”</p>
+
+<p>Han er nihilist i sin yderste konsekvens og en nytolkning af Marquis de Sade med sine sadistiske eksperimenter: Hvad står tilbage, når verden ikke har nogen værdi? En søgen efter at nedbryde alle fysiske bånd og grænser? En grænseoverskridende søgen efter kærligheden? Det virker på én gang langt ude og meget plausibelt på et teoretisk plan.</p>
+
+<p>Og den kvindelige hovedperson, der går med til at underkaste sig så fuldstændig: Er hun i virkeligheden den stærkeste af de to? Fordi hun er stærk nok til på det nærmeste at udslette sig selv på trods af kroppens mekaniske selvopholdelsesdrift? I begyndelsen havde jeg meget svært ved at acceptere, at romanen ikke indledes med at overbevise læseren om, hvorfor de elskende overhovedet er elskende. Også selv om jeg sagtens kunne forstå, at det ikke er romanens ærinde at etablere en plausibel, forståelig relation de to imellem, så irriteredes jeg over min manglende forståelse af, hvorfor hun var villig til at nedbryde sine grænser så fuldstændigt for ham.</p>
+
+<p>Efter endt læsning er jeg derimod af den mening, at grunden til, at romanen kommer til at fremstå så fuldstændig overrumplende, foruroligende og overbevisende, netop er, at der ikke er lagt en forståelig forklaringsmodel ned over den, og fordi manden ikke på nogen måde afbilder en person, som jeg eller nogen anden med en nogenlunde stabil sund psyke kunne falde for.</p>
+
+<p>Var vi som læsere først blevet præsenteret for almene genkendelige følelser, så ville vi ikke tro på, at forholdet ender helt derude, hvor det gør. Så langt ude på et overdrev. Så ville vi være endt med en utroværdig gængs skildring af et almindeligt usundt forhold.</p>
+
+<p>Nu efterlades vi derimod med en stærk udforskning af en psyke på randen af et sammenbrud. På kanten af døden. I et overbevisende litterært kærlighedsgennembrud.</p>
+
+<p>Originally published by Arendse Ørding, Litteratursiden.</p>',
     'isbn' => '9788702083859',
     'credate' => '1416577066',
     'author' => 'Carsten Michael Kaa',
