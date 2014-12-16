ccf3a1a6a7c79f406173eeda0dc6977823e8a72c
Michael Søby Andersen <michael@megha.it>
2014-12-16 15:59:15 +0100

    Added announcement ctools content type for showing the announcement. REOL-18

---
+++ sites/all/modules/reol_announcement/plugins/content_types/announcement.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * announcement.tpl.php
+ * This template shows sites announcement
+ */
+
+?>
+<div class="announcement <?php echo $class; ?>">
+  <?php echo $announcement; ?>
+</div>
