8a8b594a822e5c039b61c35b44652dd5f3cd1962
Michael Søby Andersen <michael@megha.it>
2014-12-19 12:07:59 +0100

    Styled the list. REOL-79

--- sites/all/modules/reol_latest/plugins/content_types/latest.tpl.php
+++ sites/all/modules/reol_latest/plugins/content_types/latest.tpl.php
@@ -7,5 +7,11 @@
 
 ?>
 <div class="latest-titles">
-  <?php echo render($ting_objects); ?>
+  <?php $first = "first "; ?>
+  <?php foreach ($ting_objects as $k => $ting_object) : ?>
+    <?php $last = $k == count($ting_objects) - 1 ? 'last ' : ''; ?>
+    <?php $zebra = $k % 2 == 0 ? 'odd' : 'even'; ?>
+    <div class="latest-title <?php echo $first; ?><?php echo $last; ?><?php echo $zebra; ?>"><?php echo render($ting_object); ?></div>
+    <?php $first = ''; ?>
+  <?php endforeach; ?>
 </div>
