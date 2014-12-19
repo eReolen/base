7a475756a218af3298fb5654a21ea7beada6ea60
Michael Søby Andersen <michael@megha.it>
2014-12-19 13:31:34 +0100

    Changed markup to be right. REOL-79

--- sites/all/modules/reol_latest/plugins/content_types/latest.tpl.php
+++ sites/all/modules/reol_latest/plugins/content_types/latest.tpl.php
@@ -11,7 +11,7 @@
   <?php foreach ($ting_objects as $k => $ting_object) : ?>
     <?php $last = $k == count($ting_objects) - 1 ? 'last ' : ''; ?>
     <?php $zebra = $k % 2 == 0 ? 'odd' : 'even'; ?>
-    <div class="latest-title <?php echo $first; ?><?php echo $last; ?><?php echo $zebra; ?>"><?php echo render($ting_object); ?></div>
+    <a href="#" class="latest-title <?php echo $first; ?><?php echo $last; ?><?php echo $zebra; ?>"><?php echo render($ting_object); ?></a>
     <?php $first = ''; ?>
   <?php endforeach; ?>
 </div>
