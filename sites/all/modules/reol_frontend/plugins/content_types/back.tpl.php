07841694775e29c862beced0b7628eb6d1fb1055
Michael Søby Andersen <michael@megha.it>
2014-12-29 14:43:45 +0100

    Changed to better implementation. REOL-98

--- sites/all/modules/reol_frontend/plugins/content_types/back.tpl.php
+++ sites/all/modules/reol_frontend/plugins/content_types/back.tpl.php
@@ -6,4 +6,4 @@
  */
 
 ?>
-<a class="back-button" href="javascript:history.back()"><?php echo t('<< Back to previous page.'); ?></a>
+<a class="back-button" href="javascript:history.go(-1)"><?php echo t('<< Back to previous page.'); ?></a>
