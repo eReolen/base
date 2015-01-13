7673120394713a87de13c5d2bc0f802ab94be99e
Thomas Fini Hansen <fini@reload.dk>
2015-01-13 13:29:05 +0100

    Revert "Updated to how faq works. REOL-171"
    This reverts commit e934c478586c814d3aa36517441d9c2b6d752754.

--- sites/all/themes/brin/scripts/faq.js
+++ sites/all/themes/brin/scripts/faq.js
@@ -1,7 +1,9 @@
 (function($) {
   Drupal.behaviors.faq = {
     attach : function() {
-      Drupal.behaviors.faq.setActive(window.location.hash);
+      if (window.location.hash) {
+        Drupal.behaviors.faq.setActive(window.location.hash);
+      }
 
       $('.faq-header-link, .faq-question-link').click(function() {
         // Allow clicking again to collapse.
@@ -12,40 +14,23 @@
           Drupal.behaviors.faq.setActive($(this).attr('href'));
         }
       });
-
-      $('#faq-show-all').click(function(e) {
-        e.preventDefault();
-
-        $('.faq-category-answers, .faq-item').addClass('active');
-      });
     },
 
-    setActive : function(hash, second) {
-      if (second == undefined) {
-        second = false;
-      }
-
+    setActive : function(hash) {
       var split = hash.split(':');
       var elem = $(split[0]);
 
-      if (elem.length) {
-        // Set active class on questions container.
-        $('.faq-category-answers').removeClass('active');
-        elem.addClass('active');
+      // Set active class on questions container.
+      $('.faq-category-answers').removeClass('active');
+      elem.addClass('active');
 
-        // Set active class on link in menu.
-        $('.faq-questions > div').removeClass('active');
-        $('.faq-header-link[href="'+ split[0] + '"]').closest('.faq-questions > div').addClass('active');
+      // Set active class on link in menu.
+      $('.faq-questions > div').removeClass('active');
+      $('.faq-header-link[href="'+ split[0] + '"]').closest('.faq-questions > div').addClass('active');
 
-        // Set active question.
-        $('.faq-item').removeClass('active');
-        if (split[1]) {
-          $(hash.replace(':', '\\:')).addClass('active');
-        }
-      }
-      else if (!second) {
-        // Automatically choose first, if nothing else is chosen.
-        Drupal.behaviors.faq.setActive($('.faq-menu').first().find('.faq-header-link').attr('href'), true);
+      // Set active question.
+      if (split[1]) {
+        $('#' + split[1]).addClass('active');
       }
     },
   };
--- sites/all/themes/brin/templates/faq-category-questions-top.tpl.php
+++ sites/all/themes/brin/templates/faq-category-questions-top.tpl.php
@@ -5,12 +5,8 @@
  * Template file for the questions section of the FAQ page if set to show
  * categorized questions at the top of the page.
  */
-$all_link_shown = &drupal_static('show_all_shown', FALSE);
+
 ?>
-<?php if (!$all_link_shown) : ?>
-  <a href="#" id="faq-show-all"><?php echo t('Show all questions and answers'); ?></a>
-  <?php $all_link_shown = TRUE; ?>
-<?php endif; ?>
 <div class="faq-menu">
   <a class="faq-header-link" href="#category-<?php echo $term->tid; ?>">
     <span class="faq-header">
