d0fde09e770e6493400785282bbc7d73ff89b1ab
Michael Søby Andersen <michael@megha.it>
2015-12-04 11:05:49 +0100

    Made reader version configurable. Updated default reader version to 1.5.0. REOL-247

--- sites/all/modules/reol_use_loan/js/reader_standalone.js
+++ sites/all/modules/reol_use_loan/js/reader_standalone.js
@@ -1,12 +1,13 @@
 var url = document.getElementById('reader-container').getAttribute('data-url');
 var imagesUrl = document.getElementById('reader-container').getAttribute('data-images-url');
+var readerVersion = document.getElementById('reader-container').getAttribute('data-reader-version');
 var settings = {
   elementId: 'reader-container',
   streamPackageBaseUri: url,
   streamingServiceBaseUri: url + '/publicstreaming_v2/v2/',
   sampleStreamingServiceBaseUri: url + '/samplestreaming_v2/v2/',
   sessionKeyUrl: '/reol_use_loan/reader/session/renew/{0}',
-  imageBasePath: imagesUrl + '/images/1.4.4/',
+  imageBasePath: imagesUrl + '/images/' + readerVersion + '/',
   notesEnabled: false
 };
 
