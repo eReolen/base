c1460ce59cec2b3be60213d1188f0ec82ba0066a
Thomas Slott <bigslott@gmail.com>
2016-06-08 22:56:15 +0200

    Add transitions to teasers
    resolves #BREOL-7

--- sites/all/themes/wille/template.php
+++ sites/all/themes/wille/template.php
@@ -27,26 +27,13 @@ function wille_preprocess_node(&$variables, $hook) {
   }
 }
 
-// Remove Height and Width Inline Styles from Drupal Images
+/**
+ * hook_preprocess_image().
+ *
+ * Remove Height and Width Inline Styles from Drupal Images.
+ */
 function wille_preprocess_image(&$variables) {
   foreach (array('width', 'height') as $key) {
     unset($variables[$key]);
   }
-}
-
-function hex2rgb($hex) {
-   $hex = str_replace("#", "", $hex);
-
-   if(strlen($hex) == 3) {
-      $r = hexdec(substr($hex,0,1).substr($hex,0,1));
-      $g = hexdec(substr($hex,1,1).substr($hex,1,1));
-      $b = hexdec(substr($hex,2,1).substr($hex,2,1));
-   } else {
-      $r = hexdec(substr($hex,0,2));
-      $g = hexdec(substr($hex,2,2));
-      $b = hexdec(substr($hex,4,2));
-   }
-   $rgb = array($r, $g, $b);
-   //return implode(",", $rgb); // returns the rgb values separated by commas
-   return $rgb; // returns an array with the rgb values
 }
