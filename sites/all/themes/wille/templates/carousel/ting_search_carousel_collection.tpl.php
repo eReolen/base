0c4e6ebf54e0cef3b5aa157921589d0c8ea1d60a
Thomas Slott <bigslott@gmail.com>
2016-07-19 09:12:51 +0200

    Update templates

---
+++ sites/all/themes/wille/templates/carousel/ting_search_carousel_collection.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ *
+ */
+?>
+<li class="carousel-item">
+  <a href="/ting/collection/<?php print $collection->id; ?>" class="carousel-item-image"><img src="<?php print $collection->image; ?>" alt=""/></a>
+  <a href="/ting/collection/<?php print $collection->id; ?>" class="carousel-item-title"><?php print check_plain($collection->title); ?></a>
+  <a href="/ting/collection/<?php print $collection->id; ?>" class="carousel-item-creator"><?php print check_plain($collection->creator); ?></a>
+</li>
