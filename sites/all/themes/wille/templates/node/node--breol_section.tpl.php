678f7fdcaa63ba63cbbb343a20988cdb038e3d2a
Thomas Slott <bigslott@gmail.com>
2016-07-20 11:20:36 +0200

    Add specific template for node type "breol_section"
    BREOL-67

---
+++ sites/all/themes/wille/templates/node/node--breol_section.tpl.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Template for view mode default of Breol News node type.
+ */
+?>
+
+<div class="article article--breol-section">
+  <div class="article__cover-wrapper">
+
+    <div class="article__cover__overlay">
+      <div class="article__cover__content article__cover__content--breol-section">
+      <?php if (!empty($content['field_subtitle'])) : ?>
+        <?php print drupal_render($content['field_subtitle']) ?>
+      <?php endif; ?>
+      <h2 class="title"><?php print $node->title; ?></h2>
+    </div>
+    </div>
+    <div class="article__cover article__cover--breol-section"
+    <?php hide($content['field_breol_cover_image']);?>
+    <?php if (!empty($file_uri)) : ?>
+      style="background-image: url(<?php print $file_uri; ?>)"
+    <?php endif; ?>>
+    </div>
+  </div>
+  <div class="article__carousel">
+    <?php print render($content['field_carousels']); ?>
+  </div>
+</div>
