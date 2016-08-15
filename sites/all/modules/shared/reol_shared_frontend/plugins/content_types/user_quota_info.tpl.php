9fe4e50c578391153983b6947692e47938e8d943
Thomas Fini Hansen <xen@xen.dk>
2016-08-15 09:08:32 +0200

    Add shared frontend module

---
+++ sites/all/modules/shared/reol_shared_frontend/plugins/content_types/user_quota_info.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Template for user quota info.
+ *
+ * @todo library-info is a legacy class.
+ */
+
+?>
+<div class="library-info user-quota-info">
+  <h2 class="pane-title"><?php echo t('Restrictions'); ?></h2>
+<ul>
+<li><?php echo $max_ebook_loans_text; ?></li>
+<li><?php echo $max_audiobook_loans_text; ?></li>
+<li><?php echo $max_reservations_text; ?></li>
+<li><?php echo $extra_info_text; ?></li>
+</ul>
+<div>&nbsp;</div>
+</div>
