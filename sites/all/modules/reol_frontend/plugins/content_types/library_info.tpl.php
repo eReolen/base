d421b910752a2716b9a29837aba79e5986f2e03e
Thomas Fini Hansen <xen@xen.dk>
2016-08-15 08:03:27 +0200

    Clean up quota display a bit

--- sites/all/modules/reol_frontend/plugins/content_types/library_info.tpl.php
+++ sites/all/modules/reol_frontend/plugins/content_types/library_info.tpl.php
@@ -9,10 +9,10 @@
 <div class="library-info">
   <h2 class="pane-title"><?php echo t('Restrictions'); ?></h2>
   <ul>
-    <li><?php echo $max_ebook_loans; ?></li>
-    <li><?php echo $max_audiobook_loans; ?></li>
-    <li><?php echo $max_reservations; ?></li>
-    <li><?php echo $extra_info; ?></li>
+    <li><?php echo $max_ebook_loans_text; ?></li>
+    <li><?php echo $max_audiobook_loans_text; ?></li>
+    <li><?php echo $max_reservations_text; ?></li>
+    <li><?php echo $extra_info_text; ?></li>
   </ul>
   <div>&nbsp;</div>
 </div>
