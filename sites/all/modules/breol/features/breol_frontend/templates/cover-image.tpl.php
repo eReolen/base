d9fab5eafe1f5f1c42f8f54f45e32cd1194a846d
Thomas Slott <bigslott@gmail.com>
2016-08-30 12:49:04 +0200

    Implement new cover for breol_page
    breol-161

--- sites/all/modules/breol/features/breol_frontend/templates/cover-image.tpl.php
+++ sites/all/modules/breol/features/breol_frontend/templates/cover-image.tpl.php
@@ -21,13 +21,21 @@
     <?php endif; ?>>
     </div>
   </div>
+<?php elseif ($node->type === 'breol_page') : ?>
+  <div class="user-banner">
+    <div class="user-banner__content">
+      <h2 class="title"><?php print $node->title; ?></h2>
+    </div>
+    <div class="user-banner__bg"></div>
+    <div class="user-banner__overlay"></div>
+  </div>
 <?php else : ?>
   <div class="article__cover-wrapper article__cover-wrapper--<?php print $node->type?> cover <?php print $wrapper_class?>" style="background-color: <?php print $cover_background_color; ?>">
     <div class="article__cover article__cover--<?php print $node->type?>"
       <?php if (!empty($image_uri)) : ?>
         style="background-image: url(<?php print $image_uri; ?>);"
       <?php endif; ?>>
-      <?php if (in_array($node->type, array('breol_news', 'breol_section', 'breol_page'))) : ?>
+      <?php if (in_array($node->type, array('breol_news', 'breol_section',))) : ?>
           <div class="article__cover__overlay"></div>
       <?php endif; ?>
       <div class="article__cover__content article__cover__content--<?php print $node->type?>">
@@ -36,9 +44,7 @@
             <?php print t('Category'); ?>
           </div>
         <?php endif; ?>
-        <?php if ($node->type !== 'breol_page') : ?>
-          <h2 class="title"><?php print $node->title; ?></h2>
-        <?php endif; ?>
+        <h2 class="title"><?php print $node->title; ?></h2>
         <?php if(!empty($body)) : ?>
           <?php print($body); ?>
         <?php endif; ?>
--- sites/all/themes/wille/templates/node/node--breol_page.tpl.php
+++ sites/all/themes/wille/templates/node/node--breol_page.tpl.php
@@ -5,9 +5,6 @@
  * Template for view mode default of Breol Page node type.
  */
 ?>
-
 <div class="article article--breol_page">
-  <!-- This section is empty for now, because panels do the rest. -->
-  <h2 class="title article--breol_page--title"><?php print $title; ?></h2>
   <?php print render($content['body']); ?>
 </div>
