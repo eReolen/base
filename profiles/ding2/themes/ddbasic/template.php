b3bb25b1daf58b9de1c3db19e2681202bf82431c
Thomas Fini Hansen <xen@xen.dk>
2016-08-31 09:47:04 +0200

    Adjust ddbasic to the new field_group

--- profiles/ding2/themes/ddbasic/template.php
+++ profiles/ding2/themes/ddbasic/template.php
@@ -924,11 +924,11 @@ function ddbasic_process_page(&$vars) {
 }
 
 /**
- * Implements hook_preprocess_ting_object().
+ * Implements hook_process_ting_object().
  *
  * Adds wrapper classes to the different groups on the ting object.
  */
-function ddbasic_preprocess_ting_object(&$vars) {
+function ddbasic_process_ting_object(&$vars) {
   if (isset($vars['elements']['#view_mode']) && $vars['elements']['#view_mode'] == 'full') {
     switch ($vars['elements']['#entity_type']) {
       case 'ting_object':
