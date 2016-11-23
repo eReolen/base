67b91db57b0e0743f457a44a9c9529dd67873936
Thomas Slott <bigslott@gmail.com>
2016-11-23 15:00:35 +0100

    Add timestamp to js files and load it inline
    REOL-341

--- sites/all/modules/reol_use_loan/plugins/content_types/read.tpl.php
+++ sites/all/modules/reol_use_loan/plugins/content_types/read.tpl.php
@@ -7,6 +7,7 @@
  */
 
 ?>
+<script src="<?php print($js_publizon_url); ?>"></script>
 <div class="reader">
   <div class="reader__inner">
     <a class="reader__back-button" href="javascript:history.go(-1)"><?php print t('back'); ?></a>
