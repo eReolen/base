e9ee3ecfe0922a5212a06b6489fdeb91a8659c6d
Thomas Fini Hansen <xen@xen.dk>
2017-08-24 10:03:18 +0200

    Add login link and notifications
    Ref ERTO-7

---
+++ sites/all/themes/orwell/scripts/orwell.main.js
@@ -0,0 +1,23 @@
+/**
+ * @file
+ * Main javascript file for Pratchett theme.
+ */
+
+(function ($) {
+
+  /**
+   * Make login link toggle login form.
+   */
+  Drupal.behaviors.loginFoldout = {
+    attach: function (context) {
+      // Only add the click-toggle behavior if we have a login form to toggle.
+      if ($('.js-login-form').length) {
+        $('.js-login-link', context).click(function (e) {
+          e.preventDefault();
+          $('.pane-user-login').toggle();
+        })
+      }
+    }
+  };
+
+})(jQuery);
