07a27dfd3c4dfa63e86e1e6ce922d961b5d8a759
Thomas Slott <bigslott@gmail.com>
2017-08-31 10:02:55 +0200

    Add search to header
    REOL-9

---
+++ sites/all/themes/pratchett/template.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Implements hook_preprocess_html().
+ */
+function pratchett_preprocess_html(&$variables) {
+
+  // Adding Viewport to HTML Header.
+  $viewport = array(
+      '#tag' => 'meta',
+      '#attributes' => array(
+      'name' => 'viewport',
+      'content' => 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+    ),
+  );
+
+  drupal_add_html_head($viewport, 'viewport');
+}
