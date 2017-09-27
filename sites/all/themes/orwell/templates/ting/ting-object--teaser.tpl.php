74901fcb813e251314939de82f417646471d688a
Thomas Slott <bigslott@gmail.com>
2017-09-27 12:38:12 +0200

    Theming of search result - initial theming
    ERTO-27

--- sites/all/themes/orwell/templates/ting/ting-object--teaser.tpl.php
+++ sites/all/themes/orwell/templates/ting/ting-object--teaser.tpl.php
@@ -8,30 +8,36 @@
  * - $object: The TingClientObject instance we're rendering.
  * - $content: Render array of content.
  */
+ // dpm($content);
 ?>
-<div class="material material--teaser <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <div class="material__cover material__cover--teaser">
-    <?php echo render($content['group_ting_object_teaser_left']['ting_cover']); ?>
+
+<?php if (!empty($content['ting_primary_object'])) : ?>
+  <?php print render($content['ting_primary_object']); ?>
+<?php else : ?>
+  <div class="material material--teaser <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+    <div class="material__cover material__cover--teaser">
+      <?php echo render($content['group_ting_object_teaser_left']['ting_cover']); ?>
+    </div>
+    <div class="material__content text">
+      <div class="material__rating">
+        <?php echo render($content['group_ting_object_teaser_right']['ding_entity_rating_action']); ?>
+        <?php echo render($content['group_ting_object_teaser_right']['ding_entity_rating_result']); ?>
+      </div>
+      <div class="material__title">
+        <?php echo render($content['group_ting_object_teaser_right']['ting_title']); ?>
+      </div>
+      <div class="material__author">
+        <?php echo render($content['group_ting_object_teaser_right']['ting_author']); ?>
+      </div>
+      <div class="material__abstract">
+        <?php echo render($content['group_ting_object_teaser_right']['ting_abstract']); ?>
+      </div>
+      <div class="material__subjects">
+        <?php echo render($content['group_ting_object_teaser_right']['ting_subjects']); ?>
+      </div>
+      <div class="material__series material__series--desktop">
+        <?php echo render($content['group_ting_object_teaser_right']['ting_series']); ?>
+      </div>
+    </div>
   </div>
-  <div class="material__content text">
-    <div class="material__rating">
-      <?php echo render($content['group_ting_object_teaser_right']['ding_entity_rating_action']); ?>
-      <?php echo render($content['group_ting_object_teaser_right']['ding_entity_rating_result']); ?>
-    </div>
-    <div class="material__title">
-      <?php echo render($content['group_ting_object_teaser_right']['ting_title']); ?>
-    </div>
-    <div class="material__author">
-      <?php echo render($content['group_ting_object_teaser_right']['ting_author']); ?>
-    </div>
-    <div class="material__abstract">
-      <?php echo render($content['group_ting_object_teaser_right']['ting_abstract']); ?>
-    </div>
-    <div class="material__subjects">
-      <?php echo render($content['group_ting_object_teaser_right']['ting_subjects']); ?>
-    </div>
-    <div class="material__series material__series--desktop">
-      <?php echo render($content['group_ting_object_right_column']['ting_series']); ?>
-    </div>
-  </div>
-</div>
+<?php endif; ?>
