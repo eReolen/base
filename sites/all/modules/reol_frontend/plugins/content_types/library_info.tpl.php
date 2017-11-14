3721806fc76b829a30361aadf6c865535dfe727f
Thomas Fini Hansen <xen@xen.dk>
2017-11-14 10:12:36 +0100

    Clean up quota display

--- sites/all/modules/reol_frontend/plugins/content_types/library_info.tpl.php
+++ sites/all/modules/reol_frontend/plugins/content_types/library_info.tpl.php
@@ -1,18 +1,13 @@
 <?php
+
 /**
  * @file
- * quick_guide.tpl.php
- * This template shows the quick guide.
+ * Theme quota information.
  */
-
 ?>
-<div class="library-info">
-  <h2 class="pane-title"><?php echo t('Restrictions'); ?></h2>
-  <ul>
-    <li><?php echo $max_ebook_loans_text; ?></li>
-    <li><?php echo $max_audiobook_loans_text; ?></li>
-    <li><?php echo $max_reservations_text; ?></li>
-    <li><?php echo $extra_info_text; ?></li>
-  </ul>
-  <div>&nbsp;</div>
-</div>
+<ul>
+  <li><?php echo $max_ebook_loans_text; ?></li>
+  <li><?php echo $max_audiobook_loans_text; ?></li>
+  <li><?php echo $max_reservations_text; ?></li>
+  <li><?php echo $extra_info_text; ?></li>
+</ul>
