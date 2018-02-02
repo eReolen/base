3e9242896ff0def21ce382d2be13ec31355bedd0
Jakob Larsen <jakob@revealit.dk>
2018-02-02 10:40:46 +0100

    REOL-478 CC fixes

--- sites/all/modules/reol_search/js/reol_search.search_form.js
+++ sites/all/modules/reol_search/js/reol_search.search_form.js
@@ -1,17 +1,25 @@
+/**
+ * @file
+ * Behaviors for reol_search.
+ */
+
+
 (function ($) {
 
-/**
- * Add auto submit on change for search block form radios.
- */
-Drupal.behaviors.reolSearchForm = {
-  attach: function(context, settings) {
-    $(context).find("#search-block-form #edit-search-type input").change(function(event) {
-      // Kill potential Drupal alerting about ajax being stopped due to the
-      // form being submitted.
-      window.alert = function(text) {}
-      $(this).parents('form').attr('action', $(this).val()).submit();
-    }).filter(':checked').parent().addClass('selected');
-  }
-};
+  'use strict';
+
+  /**
+   * Add auto submit on change for search block form radios.
+   */
+  Drupal.behaviors.reolSearchForm = {
+    attach: function (context, settings) {
+      $(context).find('#search-block-form #edit-search-type input').change(function (event) {
+        // Kill potential Drupal alerting about ajax being stopped due to the
+        // form being submitted.
+        window.alert = function (text) {};
+        $(this).parents('form').attr('action', $(this).val()).submit();
+      }).filter(':checked').parent().addClass('selected');
+    }
+  };
 
 }(jQuery));
