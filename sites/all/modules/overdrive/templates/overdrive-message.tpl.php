62ee198b540b34d645eb2bc30f9e075b78b9191b
Jesper Kristensen <cableman@linuxdev.dk>
2018-09-28 15:03:31 +0200

    ER-82: Added tranlation context - overdrive

--- sites/all/modules/overdrive/templates/overdrive-message.tpl.php
+++ sites/all/modules/overdrive/templates/overdrive-message.tpl.php
@@ -8,10 +8,10 @@
   <?php echo $message; ?>
   <div class="actions">
     <a class="action-button" data-modal-name="<?php echo $modal_id; ?>" target="_blank" href="<?php echo $uri; ?>">
-      <?php echo t('Borrow it at eReolen Global') ?>
+      <?php echo t('Borrow it at eReolen Global', array(), array('context' => 'overdrive')) ?>
     </a>
     <a class="action-button modal-close" data-modal-name="<?php echo $modal_id; ?>" href="#">
-      <?php echo t('Cancel'); ?>
+      <?php echo t('Cancel', array(), array('context' => 'overdrive')); ?>
     </a>
   </div>
 </div>
