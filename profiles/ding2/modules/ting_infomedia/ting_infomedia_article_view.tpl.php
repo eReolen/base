93a1b77684dde3cbe9ee3bd6a1fe1e922040b212
Jesper Kristensen <cableman@linuxdev.dk>
2018-10-25 15:33:27 +0200

    ER-125: First attempt on module clean up and removale

--- profiles/ding2/modules/ting_infomedia/ting_infomedia_article_view.tpl.php
+++
@@ -1,6 +0,0 @@
-<div  class="infomedia-article">
-  <div class="infomedia-image">
-    <?php print theme('image', array('path' => drupal_get_path('module', 'ting_infomedia') . '/images/ting_infomedia_logo.jpg')); ?>
-  </div>
-  <?php echo $variables['element']['#markup']; ?>
-</div>
