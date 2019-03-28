386e67d9556d9e9a10503b77136ead87673b5d6c
Jesper Kristensen <cableman@linuxdev.dk>
2019-03-28 16:41:41 +0100

    ER-510: Added missing documentation

--- sites/all/modules/reol_lazy_image/js/reol_lazy_image.js
+++ sites/all/modules/reol_lazy_image/js/reol_lazy_image.js
@@ -1,10 +1,23 @@
+/**
+ * @file
+ * Lazy load images with the 'js-lazy' class.
+ */
 (function($) {
 
+  // Obverser configuration.
   let options = {
     root: null,
     rootMargin: '0px'
   };
 
+  /**
+   * Observer instersection object.
+   *
+   * Swaps the placeholder image with the real image when image comes into view
+   * in the browser.
+   *
+   * @type {IntersectionObserver}
+   */
   let imageObserver = new IntersectionObserver(function imageHandler(entries, observer) {
     entries.forEach(function (entry) {
       if (entry.isIntersecting) {
@@ -18,23 +31,26 @@
     });
   }, options);
 
+  /**
+   * Attached observer to images.
+   *
+   * This is re-attached every time the carousal or other drupal ajax event is
+   * completed.
+   *
+   * @type {{attach: Drupal.behaviors.reader.attach}}
+   */
   Drupal.behaviors.reader = {
     attach : function(context, settings) {
-
-      console.log('ATT');
-
-      //setTimeout(function () {
-        $('.js-lazy').once('lazy-loading', function (index, image) {
-          if ('IntersectionObserver' in window) {
-            imageObserver.observe(image);
-          }
-          else {
-            image.src = image.dataset.src;
-          }
-        });
-      //}, 1000);
-
-
+      $('.js-lazy').once('lazy-loading', function (index, image) {
+        if ('IntersectionObserver' in window) {
+          imageObserver.observe(image);
+        }
+        else {
+          // If the browser don't support observer pattern simple load all
+          // images now.
+          image.src = image.dataset.src;
+        }
+      });
     }
   };
 })(jQuery);
