72c604baa3c06db641782cec3b2b31bdfb91ccbb
Jesper Kristensen <cableman@linuxdev.dk>
2019-10-03 12:14:35 +0200

    DDBTEAM-260: New reader/play setup

---
+++ sites/all/modules/reol_use_loan/templates/html.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ */
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="ltr" <?php print $rdf_namespaces; ?>>
+
+<head profile="http://www.w3.org/1999/xhtml/vocab"">
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<?php print drupal_render($content); ?>
+<?php print $scripts; ?>
+</body>
+</html>
---
+++ sites/all/modules/reol_use_loan/templates/read.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Default e-book reader template.
+ */
+?>
+<div class="reader">
+  <div class="reader__inner">
+    <div
+      <?php if (isset($retailer_order_number)) : ?>
+        data-id="<?php echo $retailer_order_number; ?>"
+      <?php elseif (isset($isbn)) : ?>
+        data-isbn="<?php echo $isbn; ?>"
+      <?php endif; ?>
+        id="reader-container" data-reader-version="<?php echo $reader_version ?>"
+        data-url="<?php echo $publizon_reader_stream_url; ?>"
+        data-images-url="<?php echo $publizon_reader_url; ?>">
+    </div>
+  </div>
+</div>
