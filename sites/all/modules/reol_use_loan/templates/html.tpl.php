a4221be86ca09848e6cab0ecf259c7ab9a79086b
Jesper Kristensen <cableman@linuxdev.dk>
2019-10-30 15:30:18 +0100

    ER-604: Fixed undefined index

--- sites/all/modules/reol_use_loan/templates/html.tpl.php
+++ sites/all/modules/reol_use_loan/templates/html.tpl.php
@@ -3,7 +3,7 @@
  * @file
  */
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="ltr" <?php print $rdf_namespaces; ?>>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="ltr">
 
 <head profile="http://www.w3.org/1999/xhtml/vocab"">
   <?php print $head; ?>
