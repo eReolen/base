3e7772edea1ce51c7affb51d5d045d13da32a313
Mikkel Ricky <rimi@aarhus.dk>
2021-04-14 13:03:05 +0200

    ER-1133: Updated viewport to not prevent zoom

--- sites/all/themes/pratchett/template.php
+++ sites/all/themes/pratchett/template.php
@@ -41,7 +41,7 @@ function pratchett_preprocess_html(&$variables) {
     '#tag' => 'meta',
     '#attributes' => array(
       'name' => 'viewport',
-      'content' => 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+      'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no',
     ),
   );
 
--- sites/all/themes/wille/template.php
+++ sites/all/themes/wille/template.php
@@ -34,17 +34,6 @@ function wille_preprocess_html(&$variables) {
     $css = 'body {background-color: ' . $color . '} .organic-element--content .organic-svg  {fill: ' . $color . ' !important}';
     drupal_add_css($css, 'inline');
   }
-
-  // Adding Viewport to HTML Header.
-  $viewport = array(
-    '#tag' => 'meta',
-    '#attributes' => array(
-      'name' => 'viewport',
-      'content' => 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
-    ),
-  );
-
-  drupal_add_html_head($viewport, 'viewport');
 }
 
 /**
