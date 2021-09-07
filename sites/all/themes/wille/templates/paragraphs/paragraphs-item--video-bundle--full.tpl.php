54884739fd183161eeba5530864a19f93fb0fa43
Mikkel Ricky <rimi@aarhus.dk>
2021-09-07 17:08:53 +0200

    ER-1119: Implemented some design

--- sites/all/themes/wille/templates/paragraphs/paragraphs-item--video-bundle--full.tpl.php
+++ sites/all/themes/wille/templates/paragraphs/paragraphs-item--video-bundle--full.tpl.php
@@ -7,8 +7,15 @@
 ?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      print render($content);
-    ?>
+    <div class="video_bundle__video-wrapper">
+      <?php // @todo How do we render a raw field value? ?>
+      <h2 class="video_bundle__header"><?php print $field_video_title[0]['safe_value']; ?></h2>
+      <div class="video_bundle__video"><?php print render($content['field_video_node']); ?></div>
+      <?php if (isset($field_video_description[0]['safe_value'])): ?>
+        <div class="video_bundle__description"><?php print $field_video_description[0]['safe_value']; ?></div>
+      <?php endif ?>
+    </div>
+    <?php // @todo This will render another h2 header ?>
+    <div class="video_bundle__carousel"><?php print render($content['field_carousel']); ?></div>
   </div>
 </div>
