c9e4776f4f273c8542750b8d0cb52013841b5b0d
Mikkel Ricky <rimi@aarhus.dk>
2021-09-08 15:20:24 +0200

    ER-1119: Implemented some more design

--- sites/all/themes/wille/templates/paragraphs/paragraphs-item--video-bundle--full.tpl.php
+++ sites/all/themes/wille/templates/paragraphs/paragraphs-item--video-bundle--full.tpl.php
@@ -5,17 +5,32 @@
    * Render a video bundle paragraph.
    */
 ?>
+<?php
+  // @see https://drupal.stackexchange.com/a/32518
+  $paragraphs_item_wrapper = $paragraphs_item->wrapper();
+?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
     <div class="video_bundle__video-wrapper">
-      <?php // @todo How do we render a raw field value? ?>
-      <h2 class="video_bundle__header"><?php print $field_video_title[0]['safe_value']; ?></h2>
-      <div class="video_bundle__video"><?php print render($content['field_video_node']); ?></div>
+      <h2 class="video_bundle__title"><?php print $paragraphs_item_wrapper->field_video_title->value(); ?></h2>
+      <div class="video_bundle__video">
+        <?php
+          // @see https://www.computerminds.co.uk/articles/rendering-drupal-7-fields-right-way
+          $element = field_view_field('paragraphs_item', $paragraphs_item, 'field_video_node', 'teaser');
+          print render($element);
+        ?>
+      </div>
       <?php if (isset($field_video_description[0]['safe_value'])): ?>
-        <div class="video_bundle__description"><?php print $field_video_description[0]['safe_value']; ?></div>
+        <div class="video_bundle__description"><?php print $paragraphs_item_wrapper->field_video_description->value(); ?></div>
       <?php endif ?>
     </div>
     <?php // @todo This will render another h2 header ?>
-    <div class="video_bundle__carousel"><?php print render($content['field_carousel']); ?></div>
+    <div class="video_bundle__carousel material-carousel-colored">
+      <?php
+        // @see https://www.computerminds.co.uk/articles/rendering-drupal-7-fields-right-way
+        $element = field_view_field('paragraphs_item', $paragraphs_item, 'field_carousel', 'embedded');
+        print render($element);
+      ?>
+    </div>
   </div>
 </div>
