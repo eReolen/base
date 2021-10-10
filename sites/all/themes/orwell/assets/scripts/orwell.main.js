ba5ecff9ed6756485a6cd7853e3c14018ff62b0a
Mikkel Ricky <rimi@aarhus.dk>
2021-10-10 17:57:46 +0200

    ER-1219: Added category list templates and styling

--- sites/all/themes/orwell/assets/scripts/orwell.main.js
+++ sites/all/themes/orwell/assets/scripts/orwell.main.js
@@ -144,4 +144,68 @@
     });
   });
 
+  /**
+   * Toggle secondary items in category lists.
+   *
+   * @see https://css-tricks.com/using-css-transitions-auto-dimensions/#aa-technique-3-javascript
+   */
+  $(function () {
+    function collapseSection(element) {
+      // get the height of the element's inner content, regardless of its actual size
+      var sectionHeight = element.scrollHeight;
+
+      // temporarily disable all css transitions
+      var elementTransition = element.style.transition;
+      element.style.transition = '';
+
+      // on the next frame (as soon as the previous style change has taken effect),
+      // explicitly set the element's height to its current pixel height, so we
+      // aren't transitioning out of 'auto'
+      requestAnimationFrame(function () {
+        element.style.height = sectionHeight + 'px';
+        element.style.transition = elementTransition;
+
+        // on the next frame (as soon as the previous style change has taken effect),
+        // have the element transition to height: 0
+        requestAnimationFrame(function () {
+          element.style.height = 0 + 'px';
+        });
+      });
+
+      // mark the section as "currently collapsed"
+      /* element.setAttribute('data-collapsed', 'true'); */
+      element.parentNode.classList.remove('expanded')
+    }
+
+    function expandSection(element) {
+      // get the height of the element's inner content, regardless of its actual size
+      var sectionHeight = element.scrollHeight;
+
+      // have the element transition to the height of its inner content
+      element.style.height = sectionHeight + 'px';
+
+      // when the next css transition finishes (which should be the one we just triggered)
+      element.addEventListener('transitionend', function (e) {
+        // remove this event listener so it only gets triggered once
+        element.removeEventListener('transitionend', arguments.callee);
+
+        // remove "height" from the element's inline styles, so it can return to its initial value
+        element.style.height = null;
+      });
+
+      // mark the section as "currently not collapsed"
+      /* element.setAttribute('data-collapsed', 'false'); */
+      element.parentNode.classList.add('expanded')
+    }
+
+    $('.paragraphs-item-category-list .show-more-items').on('click', function() {
+      var section = $(this).closest('.secondary-items').find('.content').get(0)
+      expandSection(section)
+    })
+
+    $('.paragraphs-item-category-list .show-fewer-items').on('click', function() {
+      var section = $(this).closest('.secondary-items').find('.content').get(0)
+      collapseSection(section)
+    })
+  })
 })(jQuery);
