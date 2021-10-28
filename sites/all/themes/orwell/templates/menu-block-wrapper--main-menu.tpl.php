c764601d88da672456cd28b7656aff29add0d204
Jesper Pedersen <pejes@aarhus.dk>
2021-10-28 14:31:26 +0200

    ER-1218: WIP create markup and styling from design mockup

---
+++ sites/all/themes/orwell/templates/menu-block-wrapper--main-menu.tpl.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Render top menu with icons
+ *
+ */
+?>
+
+<div class="menu-block-wrapper menu-block-ctools-main-menu-1 menu-name-main-menu parent-mlid-0 menu-level-1">
+  <ul class="menu">
+    <li class="menu-item">
+      <a href="/content/inspiration">
+        <span class="menu-item_icon">
+          <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="lightbulb" class="" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M176 0C73.05 0-.12 83.54 0 176.24c.06 44.28 16.5 84.67 43.56 115.54C69.21 321.03 93.85 368.68 96 384l.06 75.18c0 3.15.94 6.22 2.68 8.84l24.51 36.84c2.97 4.46 7.97 7.14 13.32 7.14h78.85c5.36 0 10.36-2.68 13.32-7.14l24.51-36.84c1.74-2.62 2.67-5.7 2.68-8.84L256 384c2.26-15.72 26.99-63.19 52.44-92.22C335.55 260.85 352 220.37 352 176 352 78.8 273.2 0 176 0zm47.94 454.31L206.85 480h-61.71l-17.09-25.69-.01-6.31h95.9v6.31zm.04-38.31h-95.97l-.07-32h96.08l-.04 32zm60.4-145.32c-13.99 15.96-36.33 48.1-50.58 81.31H118.21c-14.26-33.22-36.59-65.35-50.58-81.31C44.5 244.3 32.13 210.85 32.05 176 31.87 99.01 92.43 32 176 32c79.4 0 144 64.6 144 144 0 34.85-12.65 68.48-35.62 94.68zM176 64c-61.75 0-112 50.25-112 112 0 8.84 7.16 16 16 16s16-7.16 16-16c0-44.11 35.88-80 80-80 8.84 0 16-7.16 16-16s-7.16-16-16-16z"></path></svg>
+        </span>
+        Inspiration
+      </a>
+    </li>
+    <li class="menu-item use-ajax ajax-processed">
+      <a href="/login/nojs">
+        <span class="menu-item_icon">
+          <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="user" class="" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M313.6 288c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4zM416 464c0 8.8-7.2 16-16 16H48c-8.8 0-16-7.2-16-16v-41.6C32 365.9 77.9 320 134.4 320c19.6 0 39.1 16 89.6 16 50.4 0 70-16 89.6-16 56.5 0 102.4 45.9 102.4 102.4V464zM224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm0-224c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z"></path></svg>
+        </span>
+        Log ind
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="/content/faq">
+        <span class="menu-item_icon">
+          <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="question-circle" class="" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28zm7.67-24h-16c-6.627 0-12-5.373-12-12v-.381c0-70.343 77.44-63.619 77.44-107.408 0-20.016-17.761-40.211-57.44-40.211-29.144 0-44.265 9.649-59.211 28.692-3.908 4.98-11.054 5.995-16.248 2.376l-13.134-9.15c-5.625-3.919-6.86-11.771-2.645-17.177C185.658 133.514 210.842 116 255.67 116c52.32 0 97.44 29.751 97.44 80.211 0 67.414-77.44 63.849-77.44 107.408V304c0 6.627-5.373 12-12 12zM256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8z"></path></svg>
+        </span>
+        Hjælp
+      </a>
+    </li>
+    <!-- <li class="first leaf menu-mlid-3414"><a href="/content/inspiration">Inspiration</a></li>
+    <li class="leaf has-children menu-mlid-3421"><a href="/content/eboeger">E-bøger</a></li>
+    <li class="leaf has-children menu-mlid-3422"><a href="/content/lydboeger">Lydbøger</a></li>
+    <li class="leaf menu-mlid-4739"><a href="/content/podcasts">Podcasts</a></li>
+    <li class="leaf menu-mlid-3910"><a href="/content/boeger-paa-engelsk">Bøger på andre sprog</a></li>
+    <li class="last leaf menu-mlid-2312"><a href="/login/nojs" class="user-login-link use-ajax ajax-processed">Log ind</a></li> -->
+  </ul>
+</div>
