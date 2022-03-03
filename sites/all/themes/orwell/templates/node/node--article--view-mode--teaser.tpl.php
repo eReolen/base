90745f0ecbc3fb9c5a29fe82ad7a2daeab728291
Jesper Pedersen <pejes@aarhus.dk>
2022-03-03 16:52:41 +0100

    ER-1297: Add new styling for teaser with promoted materials

--- sites/all/themes/orwell/templates/node/node--article--view-mode--teaser.tpl.php
+++ sites/all/themes/orwell/templates/node/node--article--view-mode--teaser.tpl.php
@@ -5,35 +5,49 @@
  * Teaser view for articles.
  */
 ?>
-<div class="article article--teaser <?php print $classes; ?>">
+<div class="article--teaser__wrapper" >
+
+  <div class="article--teaser__background-image">
+    <?php print render($content['field_ding_news_list_image']); ?>
+  </div>
+
   <?php if (!empty($node_url)): ?>
   <a href="<?php print $node_url; ?>">
   <?php endif; ?>
-    <div class="article--teaser__left article--teaser__info">
-      <div class="article--teaser__info__inner">
-        <h2 class="article--teaser__title"><?php print $title; ?></h2>
-        <div class="article--teaser__body">
-          <?php print render($content['body']); ?>
-        </div>
-        <button class="article--teaser__read-more">
-          <?php print(t('read article'))?>
-        </button>
-      </div>
-    </div>
 
-    <div class="article--teaser__right <?php print '#000000' === ($background_contrast_color ?? NULL) ? 'color-dark' : 'color-light' ?>" style="background-color: <?php print $background_color ?? 'none' ?>">
-      <?php if (isset($subject)): ?>
-        <div class="subject"><?php print $subject; ?></div>
-      <?php endif ?>
+    <div class="article--teaser <?php print $classes; ?>">
+
+      <div class="article--teaser__cover">
+        <?php print render($content['field_ding_news_list_image']); ?>
+      </div>
+
+      <div class="article--teaser__subject <?php print '#000000' === ($background_contrast_color ?? NULL) ? 'color-dark' : 'color-light' ?>" style="background-color: <?php print $background_color ?? 'none' ?>">
+        <?php if (isset($subject)): ?>
+          <div class="subject"><?php print $subject; ?></div>
+        <?php endif ?>
+      </div>
+
+      <h2 class="article--teaser__title"><?php print $title; ?></h2>
+
+      <div class="article--teaser__body">
+        <?php print render($content['body']); ?>
+      </div>
+
+      <div class="article--teaser__read-more">
+        <button><?php print(t('read article'))?></button>
+      </div>
+
       <?php if (isset($promoted_materials_covers)): ?>
-        <div class="article--teaser__covers">
+        <div class="article--teaser__promoted-materials">
           <?php foreach ($promoted_materials_covers as $cover): ?>
             <?php print($cover); ?>
           <?php endforeach; ?>
         </div>
       <?php endif; ?>
     </div>
+
   <?php if (!empty($node_url)): ?>
   </a>
   <?php endif; ?>
+
 </div>
