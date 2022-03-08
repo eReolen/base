78a3a710b0e5c6b7e6d009f2a3d4d5dde1f4402a
Jesper Pedersen <pejes@aarhus.dk>
2022-03-08 09:28:17 +0100

    ER-1278: Cleanup - Revert Prettier code standards

--- sites/all/themes/orwell/assets/scripts/orwell.main.js
+++ sites/all/themes/orwell/assets/scripts/orwell.main.js
@@ -3,27 +3,25 @@
  * Main javascript file for Pratchett theme.
  */
 
-(function ($) {
-  "use strict";
+ (function ($) {
+  'use strict';
 
   // Cookie set/get functions.
   function setCookie(c_name, value, exdays) {
     var exdate = new Date();
     exdate.setDate(exdate.getDate() + exdays);
-    var c_value =
-      escape(value) +
-      (exdays === null ? "" : "; expires=" + exdate.toUTCString());
-    document.cookie = c_name + "=" + c_value;
+    var c_value = escape(value) + ((exdays === null) ? '' : ('; expires=' + exdate.toUTCString()));
+    document.cookie = c_name + '=' + c_value;
   }
   function getCookie(c_name) {
     var i;
     var x;
     var y;
-    var ARRcookies = document.cookie.split(";");
+    var ARRcookies = document.cookie.split(';');
     for (i = 0; i < ARRcookies.length; i++) {
-      x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
-      y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
-      x = x.replace(/^\s+|\s+$/g, "");
+      x = ARRcookies[i].substr(0, ARRcookies[i].indexOf('='));
+      y = ARRcookies[i].substr(ARRcookies[i].indexOf('=') + 1);
+      x = x.replace(/^\s+|\s+$/g, '');
       if (x === c_name) {
         return unescape(y);
       }
@@ -35,42 +33,42 @@
    */
   Drupal.behaviors.searchResultsGridToggle = {
     attach: function (context) {
-      var cookieName = "eReol_2__searchResultArrangement";
+      var cookieName = 'eReol_2__searchResultArrangement';
       var expires = 1;
       // Determine if we're on results page.
-      if ($(".search-results").length && $(".panel-col-first").length) {
-        var list_toggle = $(
-          '<div class="arrangement-toggles"><div class="arrangement-toggle toggle-list"></div><div class="arrangement-toggle toggle-grid"></div></div>'
-        );
-        $(".panel-col-first").prepend(list_toggle);
+      if ($('.search-results').length && $('.panel-col-first').length) {
+        var list_toggle = $('<div class="arrangement-toggles"><div class="arrangement-toggle toggle-list"></div><div class="arrangement-toggle toggle-grid"></div></div>');
+        $('.panel-col-first').prepend(list_toggle);
 
-        var $toggle = $(".arrangement-toggle");
+        var $toggle = $('.arrangement-toggle');
         // Set initial view to what's stored in the cookie, otherwise
         // set to list-view.
-        if (getCookie(cookieName) === "list-view") {
-          $(".search-results").addClass("list-view");
-          $(".arrangement-toggle.toggle-list").addClass("toggle-active");
-        } else {
-          $(".search-results").addClass("grid-view");
-          $(".arrangement-toggle.toggle-grid").addClass("toggle-active");
+        if (getCookie(cookieName) === 'list-view') {
+          $('.search-results').addClass('list-view');
+          $('.arrangement-toggle.toggle-list').addClass('toggle-active');
+        }
+        else {
+          $('.search-results').addClass('grid-view');
+          $('.arrangement-toggle.toggle-grid').addClass('toggle-active');
         }
         // When either toggle is clicked.
-        $toggle.on("click", function () {
+        $toggle.on('click', function () {
           var $this = $(this);
           // Visually toggle button states.
-          $(".arrangement-toggle").removeClass("toggle-active");
-          $this.addClass("toggle-active");
+          $('.arrangement-toggle').removeClass('toggle-active');
+          $this.addClass('toggle-active');
           // Set/update cookie to arrangement/view type.
-          if ($this.hasClass("toggle-list")) {
-            setCookie(cookieName, "list-view", expires);
-            $(".search-results").addClass("list-view").removeClass("grid-view");
-          } else {
-            setCookie(cookieName, "grid-view", expires);
-            $(".search-results").addClass("grid-view").removeClass("list-view");
+          if ($this.hasClass('toggle-list')) {
+            setCookie(cookieName, 'list-view', expires);
+            $('.search-results').addClass('list-view').removeClass('grid-view');
+          }
+          else {
+            setCookie(cookieName, 'grid-view', expires);
+            $('.search-results').addClass('grid-view').removeClass('list-view');
           }
         });
       }
-    },
+    }
   };
 
   /**
@@ -78,17 +76,15 @@
    */
   Drupal.behaviors.searchPageFacets = {
     attach: function (context, settings) {
-      if ($("body.page-search").length) {
-        var trigger = $(
-          '<div class="facets-trigger-wrapper"><div class="js-facets-trigger"></div><div>'
-        );
-        $(".panel-col-first").prepend(trigger);
+      if ($('body.page-search').length) {
+        var trigger = $('<div class="facets-trigger-wrapper"><div class="js-facets-trigger"></div><div>');
+        $('.panel-col-first').prepend(trigger);
 
-        trigger.on("click", function () {
-          $(".panel-col-first").find(".inside").slideToggle();
+        trigger.on('click', function () {
+          $('.panel-col-first').find('.inside').slideToggle();
         });
       }
-    },
+    }
   };
 
   /**
@@ -96,38 +92,39 @@
    */
   Drupal.behaviors.menuDropdown = {
     attach: function (context) {
-      $(".menu-level-2 li.expanded").once(function () {
+      $('.menu-level-2 li.expanded').once(function () {
         var menu = $(this);
-        var trigger = menu.find("> a");
-        var submenu = menu.find("ul");
+        var trigger = menu.find('> a');
+        var submenu = menu.find('ul');
 
-        trigger.on("click", function (e) {
+        trigger.on('click', function (e) {
           e.preventDefault();
           // Stop propagation, so we don't immediately trigger the
           // click event we attach to the body.
           e.stopPropagation();
 
           // Close any other open submenus.
-          menu.parent().find(".js-active").not(menu).click();
+          menu.parent().find('.js-active').not(menu).click();
 
-          if (!menu.hasClass("js-active")) {
-            menu.addClass("js-active");
+          if (!menu.hasClass('js-active')) {
+            menu.addClass('js-active');
             // Dismiss the submenu when the mouse leaves it.
-            submenu.on("mouseleave.ereolen", function () {
+            submenu.on('mouseleave.ereolen', function () {
               trigger.click();
             });
 
             // Add a click handler to body to dismiss the submenu.
-            $("body").on("click.ereolen", function (e) {
+            $('body').on('click.ereolen', function (e) {
               trigger.click();
             });
-          } else {
-            menu.removeClass("js-active");
-            submenu.off("mouseleave.ereolen");
-            $("body").off("click.ereolen");
+          }
+          else {
+            menu.removeClass('js-active');
+            submenu.off('mouseleave.ereolen');
+            $('body').off('click.ereolen');
           }
         });
       });
-    },
+    }
   };
 })(jQuery);
