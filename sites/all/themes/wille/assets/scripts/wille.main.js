f5730e35c7cbb064e0a6a24409585fb1c3ea54fb
Jesper Pedersen <pejes@aarhus.dk>
2022-03-16 10:48:29 +0100

    ER-1278: Apply coding standards

--- sites/all/themes/wille/assets/scripts/wille.main.js
+++ sites/all/themes/wille/assets/scripts/wille.main.js
@@ -3,223 +3,219 @@
  * Main javascript file for Wille theme.
  */
 
- (function ($) {
-
+(function ($) {
   /**
    * Detect tablets and mobile devices.
    */
   Drupal.behaviors.isDesktop = {
-    attach : function (context, settings) {
-
+    attach: function (context, settings) {
       // Add class if we are on desktop.
       if (!isMobile()) {
-        $('body').addClass('is-desktop');
+        $("body").addClass("is-desktop");
       }
-    }
+    },
   };
 
   /**
    * Toggle the footer menus.
    */
   Drupal.behaviors.footerToggle = {
-    attach : function (context, settings) {
-
-      $('.footer .pane-title', context).click(function () {
-
-        var element = $(this).next().find('ul');
+    attach: function (context, settings) {
+      $(".footer .pane-title", context).click(function () {
+        var element = $(this).next().find("ul");
 
         // Make sure the right classes are added, so we can
         // make sure the arrow points in the right direction.
-        if (element.css('display') === 'none') {
+        if (element.css("display") === "none") {
           $(element).slideDown(200);
-          $(this).addClass('open');
-          $(this).removeClass('closed');
-        }
-        else {
-          $(this).removeClass('open');
-          $(this).addClass('closed');
+          $(this).addClass("open");
+          $(this).removeClass("closed");
+        } else {
+          $(this).removeClass("open");
+          $(this).addClass("closed");
           $(element).slideUp(200);
         }
       });
-    }
+    },
   };
 
   /**
    * Toggle burger menu.
    */
   Drupal.behaviors.burgerMenu = {
-    attach : function (context, settings) {
-
+    attach: function (context, settings) {
       var phoneBreakPoint = 667;
 
-      $('.icon-menu', context).click(function () {
-        $('.topbar .menu').slideToggle(200);
+      $(".icon-menu", context).click(function () {
+        $(".topbar .menu").slideToggle(200);
       });
 
-      $('.menu-name-main-menu .menu a', context).click(function () {
+      $(".menu-name-main-menu .menu a", context).click(function () {
         if ($(window).width() < phoneBreakPoint) {
-          $('.topbar .menu').slideUp(500);
+          $(".topbar .menu").slideUp(500);
 
           // Goto top of page.
           window.scrollTo(0, 0);
         }
       });
-    }
+    },
   };
 
   /**
    * Facets.
    */
   Drupal.behaviors.facets = {
-    attach : function (context, settings) {
-
-      $('#ding-facetbrowser-form', context).each(function () {
-        $('fieldset', this).each(function () {
-
-          var dropdown = $(this).find('.fieldset-wrapper').addClass('js-processed');
+    attach: function (context, settings) {
+      $("#ding-facetbrowser-form", context).each(function () {
+        $("fieldset", this).each(function () {
+          var dropdown = $(this)
+            .find(".fieldset-wrapper")
+            .addClass("js-processed");
 
           $(this).click(function () {
-            dropdown.slideToggle(200).toggleClass('open');
+            dropdown.slideToggle(200).toggleClass("open");
           });
         });
       });
 
-      if ($('#ding-facetbrowser-form').length !== 0) {
+      if ($("#ding-facetbrowser-form").length !== 0) {
+        var html = $(
+          '<div class="js-toggle-facets">' +
+            Drupal.t("Limit search results") +
+            "</div>"
+        );
 
-        var html = $('<div class="js-toggle-facets">' + Drupal.t('Limit search results') + '</div>');
+        $(".pane-panels-mini.pane-search").prepend(html);
 
-        $('.pane-panels-mini.pane-search').prepend(html);
+        $(".js-toggle-facets").click(function () {
+          $(this).toggleClass("open");
 
-        $('.js-toggle-facets').click(function () {
-
-          $(this).toggleClass('open');
-
-          $('.pane-breol-facetbrowser').toggle();
-          $('.before-content').toggle();
+          $(".pane-breol-facetbrowser").toggle();
+          $(".before-content").toggle();
         });
       }
-    }
+    },
   };
 
   /**
    * Search drop down.
    */
   Drupal.behaviors.searchDropDown = {
-    attach : function (context, settings) {
+    attach: function (context, settings) {
       // If the block is not present in the DOM abort.
-      if ($('.pane-search-form').length === 0) {
+      if ($(".pane-search-form").length === 0) {
         return;
       }
 
       // If we are on the search page we dont want to hide the form,
       // but still don't want users to be able to follow the search link.
-      if ($('body.page-search').length !== 0) {
-
+      if ($("body.page-search").length !== 0) {
         // Show search form.
-        $('.pane-search-form').show();
+        $(".pane-search-form").show();
 
-        $('.menu-name-main-menu ul li.last a', context)
-          .click(function (event) {
-            event.preventDefault();
-          });
-      }
-      else {
-        var searchFormWrapper = $('.pane-search-form', context);
+        $(".menu-name-main-menu ul li.last a", context).click(function (event) {
+          event.preventDefault();
+        });
+      } else {
+        var searchFormWrapper = $(".pane-search-form", context);
         searchFormWrapper.hide();
 
         // We assume that search is the fifth element. This would be preferable
         // if it was more dynamic.
-        $('.menu-name-main-menu ul li.last a', context)
-          .click(function (event) {
-            event.preventDefault();
-            // The slide animation sets overflow: hidden, but the
-            // autocomplete on the search field requires overflow to
-            // be visible, so we reset it when the animation
-            // completes.
-            searchFormWrapper.slideToggle(400, function () {
-              $(this).css({overflow: 'visible'});
-            });
+        $(".menu-name-main-menu ul li.last a", context).click(function (event) {
+          event.preventDefault();
+          // The slide animation sets overflow: hidden, but the
+          // autocomplete on the search field requires overflow to
+          // be visible, so we reset it when the animation
+          // completes.
+          searchFormWrapper.slideToggle(400, function () {
+            $(this).css({ overflow: "visible" });
           });
+        });
       }
-    }
+    },
   };
 
   /**
    * Make ting object details collapsible.
    */
   Drupal.behaviors.tingObject = {
-    attach : function (context, settings) {
-
+    attach: function (context, settings) {
       var fields = [
-        '.group-material-details',
-        '.ting-object-related-item',
-        '.pane-ting-ting-object-types'
+        ".group-material-details",
+        ".ting-object-related-item",
+        ".pane-ting-ting-object-types",
       ];
 
       $(fields).each(function (id, field) {
-
         $(field).each(function (id, element) {
           $(element)
-            .addClass('ting-object-collapsible-enabled')
-            .addClass('open')
-            .find('h2')
+            .addClass("ting-object-collapsible-enabled")
+            .addClass("open")
+            .find("h2")
             .nextAll()
             .wrapAll('<div class="collapsible-content-wrapper" />');
 
-          $('.collapsible-content-wrapper').hide();
+          $(".collapsible-content-wrapper").hide();
 
-          $(element).find('h2').click(function () {
-            $(element)
-              .toggleClass('open')
-              .find('.collapsible-content-wrapper')
-              .slideToggle();
-          });
+          $(element)
+            .find("h2")
+            .click(function () {
+              $(element)
+                .toggleClass("open")
+                .find(".collapsible-content-wrapper")
+                .slideToggle();
+            });
         });
       });
-    }
+    },
   };
 
   /**
    * Detect if jquery ui-dialog is open.
    */
   Drupal.behaviors.uiDialog = {
-    attach : function (context, settings) {
-      if ($('.ui-dialog', context).css('display') == 'none' || $('.ui-dialog').length === 0) {
+    attach: function (context, settings) {
+      if (
+        $(".ui-dialog", context).css("display") == "none" ||
+        $(".ui-dialog").length === 0
+      ) {
         // Fallback.
-        $('body').removeClass('ui-dialog-is-open');
-      }
-      else {
+        $("body").removeClass("ui-dialog-is-open");
+      } else {
         // Add Class that indicates the dialog is open.
-        $('body').addClass('ui-dialog-is-open');
+        $("body").addClass("ui-dialog-is-open");
 
         // Make sure we remove the class when users close the dialog.
-        $('.ui-button-icon-primary').click(function () {
-          $('body').removeClass('ui-dialog-is-open');
+        $(".ui-button-icon-primary").click(function () {
+          $("body").removeClass("ui-dialog-is-open");
         });
       }
-    }
+    },
   };
 
   /**
    * Detect and remove empty paragraphs
    */
-     Drupal.behaviors.removeEmptyParagraphs = {
-      attach: function (context, settings) {
-        $(".ting-object-description p").each(function () {
-          if ($.trim($(this).text()) == "" && $(this).children().length == 0) {
-            $(this).hide();
-          }
-        });
-      },
-    };
+  Drupal.behaviors.removeEmptyParagraphs = {
+    attach: function (context, settings) {
+      $(".ting-object-description p").each(function () {
+        if ($.trim($(this).text()) == "" && $(this).children().length == 0) {
+          $(this).hide();
+        }
+      });
+    },
+  };
 
   /**
    * Add grid view option.
    */
   Drupal.behaviors.gridView = {
-    attach : function (context, settings) {
-      $('.pane-panels-mini.pane-search').viewPicker('.pane-ting-search-sort-form');
-    }
+    attach: function (context, settings) {
+      $(".pane-panels-mini.pane-search").viewPicker(
+        ".pane-ting-search-sort-form"
+      );
+    },
   };
 
   var resizeTimer;
@@ -228,9 +224,9 @@
     var windowWidth = window.innerWidth;
 
     if (windowWidth > 1024) {
-      $('.pane-breol-facetbrowser, .before-content').show();
+      $(".pane-breol-facetbrowser, .before-content").show();
     }
-  };
+  }
 
   // On resize, run the function and reset the timeout
   // 250 is the delay in milliseconds. Change as you see fit.
@@ -245,19 +241,18 @@
    * Detect if we are on mobile devices.
    */
   function isMobile() {
-    if (navigator.userAgent.match(/Android/i)
-      || navigator.userAgent.match(/webOS/i)
-      || navigator.userAgent.match(/iPhone/i)
-      || navigator.userAgent.match(/iPad/i)
-      || navigator.userAgent.match(/iPod/i)
-      || navigator.userAgent.match(/BlackBerry/i)
-      || navigator.userAgent.match(/Windows Phone/i)
+    if (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
     ) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
-
 })(jQuery);
